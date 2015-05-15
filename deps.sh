4269e64cea397f9b88355348e2da045d3272f917
Chris Richards <chris.richards@yellowfeather.co.uk>
2015-05-15 15:32:42 +0100

    Adds wiring-pi to depency list

--- deps.sh
+++ deps.sh
@@ -4,13 +4,4 @@ set -o errexit
 
 apt-get -q update
 apt-get -q -y upgrade
-apt-get -q -y install python build-essential python-dev python-pip
-# Install python modules
-#pip install RPi.GPIO
-dpkg -i python-rpi.gpio_0.5.8-1_armhf.deb
-
-git clone git://git.drogon.net/wiringPi
-cd wiringPi
-./build
-cd ..
 
